ed05648a84d5265241d5c2c94b9626e46f2a6f29
Taufik Nurrohman <t.nurrohman77@gmail.com>
2018-12-26 21:12:09 +0700

    Apply Static Scope

--- less/index.php
+++ less/index.php
@@ -4,7 +4,7 @@ require __DIR__ . DS . 'lot' . DS . 'worker' . DS . '@less' . DS . 'Less.php';
 require __DIR__ . DS . 'engine' . DS . 'plug' . DS . 'asset.php';
 
 Hook::set('asset:head', function($content) {
-    return $content . Hook::fire('asset.less', [Asset::less()]);
+    return $content . Hook::fire('asset.less', [Asset::less()], null, Asset::class);
 });
 
 // Add `less` to the allowed file extension(s)
