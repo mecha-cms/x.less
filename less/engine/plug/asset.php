f4614daee630fee2f0c197016b0d2e9506ed6b43
Taufik Nurrohman <t.nurrohman77@gmail.com>
2019-02-22 18:48:44 +0700

    https://github.com/mecha-cms/genome/issues/46

--- less/engine/plug/asset.php
+++ less/engine/plug/asset.php
@@ -29,7 +29,7 @@ function files(string $path): array {
 \Asset::_('.less', function($value, $key, $data) {
     extract($value, \EXTR_SKIP);
     $state = \Extend::state('asset');
-    if ($path !== false) {
+    if (isset($path)) {
         $less = new \lessc;
         $less->setFormatter('compressed');
         $less->setImportDir([dirname($path)]);
