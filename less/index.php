f3a6b624465f0645f4ff260916a1135043490f7c
Taufik Nurrohman <t.nurrohman77@gmail.com>
2020-02-06 09:14:04 +0700

    Remove Demo and Docs File(s)

--- less/index.php
+++ less/index.php
@@ -1,11 +1,11 @@
 <?php
 
 require __DIR__ . DS . 'engine' . DS . 'i' . DS . '@leafo' . DS . 'lessphp' . DS . 'lessc.inc.php';
-require __DIR__ . DS . 'engine' . DS . 'i' . DS . '@leafo' . DS . 'lessphp' . DS . 'lessify.inc.php';
+// require __DIR__ . DS . 'engine' . DS . 'i' . DS . '@leafo' . DS . 'lessphp' . DS . 'lessify.inc.php';
 
 require __DIR__ . DS . 'engine' . DS . 'plug' . DS . 'asset.php';
 require __DIR__ . DS . 'engine' . DS . 'plug' . DS . 'from.php';
-require __DIR__ . DS . 'engine' . DS . 'plug' . DS . 'to.php';
+// require __DIR__ . DS . 'engine' . DS . 'plug' . DS . 'to.php';
 
 require __DIR__ . DS . 'engine' . DS . 'r' . DS . 'file.php';
 require __DIR__ . DS . 'engine' . DS . 'r' . DS . 'hook.php';
